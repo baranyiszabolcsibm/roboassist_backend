145949b5c29a451b5e5ed2a43d4c0accb95ee6d6
baranyisz <baranyisz@otpbank.hu>
2024-03-27 23:52:50 +0100

    mnb base

---
+++ mnb_apitest.py
@@ -0,0 +1,84 @@
+import json
+import os
+import aiohttp
+import asyncio
+from PyPDF2 import PdfMerger   ##pip install PyPDF2
+import uuid
+import re
+
+basepath = "./pdftmp/"    ## global
+
+async def send_post(session, url, chatquestion): 
+    payload = {
+        "input": chatquestion
+        }
+    async with session.post(url, json=payload ) as response:
+      if response.status == 200:
+        data = await response.text()
+        print(data)
+        return data
+      else:
+        print(f"Error: {response.status}")      
+
+async def send_post_request_with_file(session, url, file_path): 
+    mpwriter = aiohttp.MultipartWriter('form-data')
+    file_name = os.path.basename(file_path)
+    with open(file_path, 'rb') as f:
+        part = mpwriter.append(f)
+        part.set_content_disposition('form-data', name='pdf_file', filename=file_name)  
+        async with session.post(url, data=mpwriter) as response:
+            resdata = await response.text()
+            print(resdata)
+            return resdata
+        
+def mergePdf(pdflist,summaryid):
+  merger = PdfMerger()
+  #pdflist = ["21.pdf", "25.pdf"]
+  for p in pdflist:
+    pdfpath=basepath+p
+    merger.append(pdfpath)
+  merger.write(basepath+"summary_{}.pdf".format(summaryid))
+  merger.close()
+
+def find_page_numbers(text):
+    pattern = r'Page (\d+)'
+    matches = re.findall(pattern, text)
+    return [int(num) for num in matches]
+
+        
+async def main():
+    chat_url = "http://91.107.238.245:8016/mnb_metadataRAG"
+    process_pdf_url = "http://91.107.238.245:8016/process-pdf/?question={}"
+    
+ 
+
+    async with aiohttp.ClientSession() as session:  
+        while True:
+          try:
+              print("Adja meg a kérdést:")
+              clv_question = input()
+              chat_response = await send_post(session,chat_url,clv_question)
+              print("Summarizer válasz: \n", chat_response)
+              json_obj = json.loads(chat_response)
+              sumid= str(uuid.uuid1())
+              mergePdf(json_obj['outputs'],sumid)
+              #for item in json_obj['outputs']:
+              #   print(basepath+item)
+                 
+
+              ##print("File feltöltés")
+              clv_file =  "C:\\Users\\szabaran\\OneDrive - Crayon Group\\source\\BMAI\\mnb_backend\\pdftmp\\summary_{}.pdf".format(sumid)  
+              print(clv_file)   
+              url_with_question = process_pdf_url.format(clv_question)
+              restxt = await send_post_request_with_file(session,url_with_question,clv_file)
+              json_obj = json.loads(restxt)
+              page_numbers = find_page_numbers(json_obj['answer'])
+              print(page_numbers)
+
+          except KeyboardInterrupt:
+              break 
+        
+    
+
+
+asyncio.run(main())
--- mnb_server.py
+++ mnb_server.py
@@ -19,7 +19,7 @@ def handle_preflight():
 
 #http://localhost:5000/todo/api/v1.0/extractPdf
 #@app.route('/todo/api/v1.0/extractPdf', methods=['GET'])
-@app.route('/bmapi/extractPdf', methods=['GET'])
+@app.route('/mnbapi/ragsearch', methods=['GET'])
 async def extractPdf():
     filename = request.args.get('filename')
     pdf_file_path = './pdftmp/'+filename 
@@ -42,55 +42,9 @@ async def extractPdf():
 
 
 
-@app.route('/bmapi/aktakerdes', methods=['GET'])
-async def send_post_chat(): 
-    collection = request.args.get('collection')
-    question = request.args.get('question')
-    resultmsg="&#x1F601 P\u00c9TER\ud83d\udc6e: A dokumentumok alapj\u00e1n a feljelent\u0151 neve Imre P\u00e9ter. Sajnos a dokumentumokban nem tal\u00e1lhat\u00f3 meg a feljelent\u0151 telefonsz\u00e1ma.";
-
-    #
-    # response = response = jsonify(resultmsg)
-    # resx = resultmsg.encode('utf-8').decode('utf-8')
-    # print(resx)
-    # response.headers.add('Access-Control-Allow-Origin', '*')
-    # response.headers['Content-Type'] = 'text/plain; charset=utf-8'
-    # return response 
-
-    question += " A dokumentum első forrását a válaszod végén ismételten add meg a következő formában is {{oldalszám}}"
-    payload = {
-        "collection": collection,
-        "prompt": question,
-        "temperature": 0
-        }
-    token = "5pBHDjr4bkNFc1xdqIMR6INLItKuPvZrf8zNdc6enlXqhy8qVO8YCYKRcdd"
-    header = {"Authorization": f"Bearer {token}"}
-    async with aiohttp.ClientSession() as session:
-        async with session.post(chat_url, json=payload, headers=header) as response:
-            if response.status == 200:
-                data = await response.text()
-                print(data)
-                resultmsg = data
-            else:
-                print(f"Error: {response.status}")  
-
-        resultmsg =resultmsg.replace("\\\"","\"")     
-        resultmsg = resultmsg.encode('ascii', 'xmlcharrefreplace').decode('utf-8')
-        resultmsg =resultmsg.replace("\n","<br />")      
-  
-        response = jsonify(resultmsg)
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        #response.headers['Content-Type'] = 'text/plain; charset=utf-8'
-        return response 
-
-    
 
 
-
-
-
-
-
-@app.route('/bmapi/uploadPdf', methods=['GET', 'POST'])   # mondket method kell mert a browser elsőször egy getet küld implicite
+@app.route('/mnbapi/uploadPdf', methods=['GET', 'POST'])   # mondket method kell mert a browser elsőször egy getet küld implicite
 async def uploadPdf():
     if request.method == 'GET':
         response_headers = {'Access-Control-Allow-Origin': '*'  }
@@ -99,7 +53,7 @@ async def uploadPdf():
         # Get file from request
         file = request.files['file']
         filename = file.filename
-        file.save('./pdftmp/'+filename)
+        file.save('./pdftmp/usr_'+filename)
         response_text = "Mentve Rendbe"
         response = jsonify(response_text)
         response.headers.add('Access-Control-Allow-Origin', '*')
@@ -124,13 +78,6 @@ async def send_post_request_with_file(session, url, file_path):
         part.set_content_disposition('form-data', name='file', filename=file_path)  
         async with session.post(url, data=mpwriter) as response:
             return await response.text()
-
-async def get_metadata_summary(session, url):
-    async with session.get(url) as response:
-        if response.status == 200:              
-            return await response.text()
-        else:             
-            raise Exception(f"Hiba a GET kérés során: {response.status}")
 
 
 def json_to_array(json_obj):
